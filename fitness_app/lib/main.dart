5973785c56b27d7a2eee67c96c8235e008a660c5
chimsdee <chimsom09@gmail.com>
2025-07-10 13:38:15 +0100

    commit

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -1,41 +1,40 @@
-import 'package:fitness_app/screens/Calculators/bmiCalculator.dart';
-import 'package:fitness_app/screens/Calculators/bmrCalculator.dart';
-import 'package:fitness_app/screens/Calculators/bodyfat_calc.dart';
-import 'package:fitness_app/screens/Calculators/hydration_calc.dart';
-import 'package:fitness_app/screens/Calculators/idealweight_calc.dart';
-import 'package:fitness_app/screens/ProfilePage/UnitsOfMeasure.dart';
-import 'package:fitness_app/screens/calculatorCategories.dart';
-import 'package:fitness_app/screens/Workouts/warmups.dart';
-import 'package:fitness_app/screens/Calculators/tdee_calc.dart';
 import 'package:flutter/material.dart';
-import 'package:fitness_app/screens/Login%20SignUp/LoginSignUp.dart';
-import 'package:fitness_app/screens/ProfilePage/Languages.dart';
-import 'package:fitness_app/screens/ProfilePage/PrivacyPolicy.dart'
-    as privacy_policy;
-import 'package:fitness_app/screens/ProfilePage/SettingsPage.dart'
-    as settings_page;
-import 'package:fitness_app/screens/ProfilePage/contactUs.dart';
-import 'package:fitness_app/screens/ProfilePage/editProfile.dart'
-    as edit_profile;
-import 'package:fitness_app/screens/ProfilePage/profilePage.dart';
-import 'package:fitness_app/screens/ProfilePage/settings_Notifications.dart';
+import 'package:fitness_app/screens/Workouts/warmups.dart';
+import 'package:fitness_app/screens/workoutCategories.dart';
 import 'package:fitness_app/screens/Workouts/advanced.dart';
 import 'package:fitness_app/screens/Workouts/beginner.dart';
-import 'package:fitness_app/screens/homeScreen/Notifications.dart';
-import 'package:fitness_app/screens/homeScreen/bottomNavigationBar.dart';
+import 'package:fitness_app/screens/ageScreen/ageScreen.dart';
+import 'package:fitness_app/screens/calculatorCategories.dart';
+import 'package:fitness_app/screens/Calculators/tdee_calc.dart';
+import 'package:fitness_app/screens/ProfilePage/Languages.dart';
+import 'package:fitness_app/screens/ProfilePage/contactUs.dart';
 import 'package:fitness_app/screens/homeScreen/homeScreen.dart';
 import 'package:fitness_app/screens/Workouts/intermediate.dart';
-import 'package:fitness_app/screens/workoutCategories.dart';
-import 'package:fitness_app/screens/OnBoardingScreen/onBoardingScreen.dart';
+import 'package:fitness_app/screens/goalScreen/goalScreen.dart';
+import 'package:fitness_app/screens/ProfilePage/profilePage.dart';
+import 'package:fitness_app/screens/homeScreen/Notifications.dart';
+import 'package:fitness_app/screens/Calculators/bodyfat_calc.dart';
 import 'package:fitness_app/screens/genderScreen/genderScreen.dart';
-import 'package:fitness_app/screens/ageScreen/ageScreen.dart';
+import 'package:fitness_app/screens/Calculators/bmrCalculator.dart';
+import 'package:fitness_app/screens/Calculators/bmiCalculator.dart';
 import 'package:fitness_app/screens/heightScreen/heightScreen.dart';
 import 'package:fitness_app/screens/weightScreen/weightScreen.dart';
-import 'package:fitness_app/screens/goalScreen/goalScreen.dart';
+import 'package:fitness_app/screens/ProfilePage/UnitsOfMeasure.dart';
+import 'package:fitness_app/screens/Calculators/hydration_calc.dart';
+import 'package:fitness_app/screens/Login%20SignUp/LoginSignUp.dart';
+import 'package:fitness_app/screens/Calculators/idealweight_calc.dart';
+import 'package:fitness_app/screens/homeScreen/bottomNavigationBar.dart';
+import 'package:fitness_app/screens/OnBoardingScreen/onBoardingScreen.dart';
+import 'package:fitness_app/screens/ProfilePage/settings_Notifications.dart';
 import 'package:fitness_app/screens/activityLevelScreen/activityLevelScreen.dart';
+import 'package:fitness_app/screens/ProfilePage/PrivacyPolicy.dart'
+    as privacy_policy;
+import 'package:fitness_app/screens/ProfilePage/editProfile.dart'
+    as edit_profile;
+import 'package:fitness_app/screens/ProfilePage/SettingsPage.dart'
+    as settings_page;
 import 'package:fitness_app/screens/ProfilePage/ProUpgradePage.dart'
     as pro_upgrade_page;
-
 // ignore: unused_import
 import 'package:fitness_app/screens/Login%20SignUp/forgotPassword.dart';
 // ignore: unused_import
@@ -92,8 +91,8 @@ class MyApp extends StatelessWidget {
         '/proUpgrade': (context) => const pro_upgrade_page.ProUpgradePage(),
       },
       debugShowCheckedModeBanner: false,
-      home: const HomepageNavbar(),
-      // home: const OnBoardingScreen(),
+      // home: const HomepageNavbar(),
+      home: const OnBoardingScreen(),
       // home: const SignUp(),
       // home: const workoutCategoriesPage(),
       //home: const NotificationPage(),
