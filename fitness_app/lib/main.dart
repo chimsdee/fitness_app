d8e2fd6731e3eb23b5aae918ae76174cde3d463a
chimsdee <chimsom09@gmail.com>
2025-09-07 14:22:30 +0100

    commit

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -48,7 +48,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of my app.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
